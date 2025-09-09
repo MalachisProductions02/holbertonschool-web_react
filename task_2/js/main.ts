b21af2265164ded14e3abff90690d02697a2dabe
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-09-09 11:12:23 -0600

    task2

---
+++ task_2/js/main.ts
@@ -0,0 +1,25 @@
+// 1. Teacher interface
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+// 2. Directors interface extends Teacher
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// 3. Crear objeto de tipo Directors
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
